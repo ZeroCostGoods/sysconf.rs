222b618369c4e5fd26569f52a7edc9686843133b
Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
2017-08-30 21:38:12 -0700

    Add high-level wrappers for memory page functions, move sysconf to raw module (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,30 @@
-extern crate libc;
+//! Query runtime configuration information.
+//!
+//! This crate provides the ability to query for various configuration information about the
+//! runtime platform such as memory page size. On POSIX systems, it makes heavy use of the
+//! [sysconf] API.
+//!
+//! [sysconf]: http://man7.org/linux/man-pages/man3/sysconf.3.html
 
-use std::result;
-use self::libc::{c_int, c_long};
+// NOTE: According to Wikipedia, "Originally, the name "POSIX" referred to IEEE Std 1003.1-1988,
+// released in 1988." This crate assumes that any behavior required by POSIX 1003.1 will be
+// properly implemented on any POSIX system. Running on a POSIX system which does not adhere to
+// these requirements will cause either an assertion failure/panic or undefined behavior.
 
-pub type Result<T> = result::Result<T, SysconfError>;
+#![cfg_attr(not(test), no_std)]
 
-#[derive(Debug)]
-pub enum SysconfError {
-    Invalid
-}
+#[cfg(test)] // In tests, we disable no_std, so core isn't automatically included
+extern crate core;
+// no-std lazy_static requires nightly
+// TODO: Use lazy_static unconditionally once it works on stable
+#[cfg(feature = "nightly")]
+#[macro_use]
+extern crate lazy_static;
 
-pub enum SysconfVariable {
-    ScArgMax = 0,
-    ScChildMax = 1,
-    ScClkTck = 2,
-    ScNgroupsMax = 3,
-    ScOpenMax = 4,
-    ScStreamMax = 5,
-    ScTznameMax = 6,
-    ScJobControl = 7,
-    ScSavedIds = 8,
-    ScRealtimeSignals = 9,
-    ScPriorityScheduling = 10,
-    ScTimers = 11,
-    ScAsynchronousIo = 12,
-    ScPrioritizedIo = 13,
-    ScSynchronizedIo = 14,
-    ScFsync = 15,
-    ScMappedFiles = 16,
-    ScMemlock = 17,
-    ScMemlockRange = 18,
-    ScMemoryProtection = 19,
-    ScMessagePassing = 20,
-    ScSemaphores = 21,
-    ScSharedMemoryObjects = 22,
-    ScAioListioMax = 23,
-    ScAioMax = 24,
-    ScAioPrioDeltaMax = 25,
-    ScDelaytimerMax = 26,
-    ScMqOpenMax = 27,
-    ScVersion = 29,
-    ScPagesize = 30,
-    ScRtsigMax = 31,
-    ScSemNsemsMax = 32,
-    ScSemValueMax = 33,
-    ScSigqueueMax = 34,
-    ScTimerMax = 35,
-    ScBcBaseMax = 36,
-    ScBcDimMax = 37,
-    ScBcScaleMax = 38,
-    ScBcStringMax = 39,
-    ScCollWeightsMax = 40,
-    ScExprNestMax = 42,
-    ScLineMax = 43,
-    ScReDupMax = 44,
-    Sc2Version = 46,
-    Sc2CBind = 47,
-    Sc2CDev = 48,
-    Sc2FortDev = 49,
-    Sc2FortRun = 50,
-    Sc2SwDev = 51,
-    Sc2Localedef = 52,
-    ScNprocessorsOnln = 84,
-    Sc2CharTerm = 95,
-    Sc2CVersion = 96,
-    Sc2Upe = 97,
-    ScXbs5Ilp32Off32 = 125,
-    ScXbs5Ilp32Offbig = 126,
-    ScXbs5LpbigOffbig = 128,
-}
+pub mod page;
+#[cfg(unix)]
+pub mod raw;
 
-pub fn sysconf(name: SysconfVariable) -> Result<c_long> {
-    match unsafe { libc::sysconf(name as c_int) } {
-        -1  => Err(SysconfError::Invalid),
-        ret => Ok(ret),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_valid_sysconf() {
-        let result = sysconf(SysconfVariable::ScPagesize);
-        assert!(result.is_ok())
-    }
-}
+pub use page::*;
+#[cfg(unix)]
+pub use raw::*;
